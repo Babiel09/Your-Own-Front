2f4f6cc9409da031cc32f5bd60f229ec0d1c6195
Gabriel Castro <estudosbabiel@gmail.com>
2025-04-20 13:39:02 -0300

    style: Made the Header fixed in the screen

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Header(){
     return(
         <header>
-                  <nav className="sticky top-0 z-50 w-full bg-white/80 backdrop-blur-lg border-b border-gray-100 shadow-sm">
+                  <nav className="top-0 z-50 w-full bg-white backdrop-blur-lg border-b border-gray-100 shadow-sm fixed">
                     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                       <div className="flex justify-between h-16">
                         <div className="flex items-center">
