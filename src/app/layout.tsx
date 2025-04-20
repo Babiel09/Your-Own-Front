1876cd3fefadd44032521ea8b17fc26fdcc29a3f
Gabriel Castro <estudosbabiel@gmail.com>
2025-04-20 13:55:59 -0300

    fixed: Made the Header give a little space to the body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
       <Header/>
+      <br />
+      <br />
         {children}
         <Footer/>
       </body>
